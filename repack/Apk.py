e19ddc076b039ed824abb7b26b5b6b1365d641bd
NickQiao <393533945@qq.com>
2017-05-07 13:43:03 +0800

    init

---
+++ repack/Apk.py
@@ -0,0 +1,12 @@
+
+class AndroidManifest(object):
+    def __init__(self, file_path):
+        self._file_path = file_path
+
+
+class Apk(object):
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    pass
